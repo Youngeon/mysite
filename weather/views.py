baa881a9a76cc0dfb8000282933c2067fe330694
Youngeon <emik219@>
2020-10-08 17:39:25 +0300

    added weather not working

--- weather/views.py
+++ weather/views.py
@@ -1,4 +1,27 @@
 from django.shortcuts import render
+import requests
+from .models import City
 
 def index(request):
-    return render(request, 'weather/index.html' )
+    appid = '146a87f980237d3343ca5522e8ab816f'
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=' + appid
+
+    
+    
+    
+    cities = City.objects.all()
+
+    all_cities = []
+
+    for city in cities:
+        res = requests.get(url.format(city.name)).json()
+        city_info = {
+            'city': city.name,
+            'temp': res["main"]["temp"],
+            'icon': res["weather"][0]["icon"]
+        }
+
+    all_cities.append(city_info) 
+
+    context = {'all_info': city_info}
+    return render(request, 'weather/index.html', context)
