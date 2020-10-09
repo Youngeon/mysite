cb075ee7798ff66aa9ec35256f7ff1dfc8133145
Youngeon <emik219@>
2020-10-09 16:45:15 +0300

    weather app without normal CSS

--- weather/views.py
+++ weather/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render
 import requests
 from .models import City
+from .forms import CityForm
+
 
 def index(request):
     appid = '146a87f980237d3343ca5522e8ab816f'
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=' + appid
 
     
-    
+    if(request.method == "POST"):
+        form = CityForm(request.POST)
+        form.save()
+
+    form = CityForm() # Erase form 
     
     cities = City.objects.all()
 
@@ -23,5 +29,5 @@ def index(request):
 
     all_cities.append(city_info) 
 
-    context = {'all_info': city_info}
+    context = {'all_info': all_cities, 'form': form}
     return render(request, 'weather/index.html', context)
