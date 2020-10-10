887e202a8ea1b7e4e5e959e1f401257d77c937f0
Youngeon <emik219@>
2020-10-10 23:33:31 +0300

    fuck rusey

--- mysite/apps/mainApp/models.py
+++ mysite/apps/mainApp/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Users(models.Model):
+    login = models.CharField(max_length = 120)
+    nickname = models.TextField() 
+    password = models.DateTimeField()
--- mysite/apps/plugins/SMTP.py
+++ mysite/apps/plugins/SMTP.py
@@ -1,29 +1,30 @@
 import smtplib
 
+class sender():
 #mail.nic.ru 587 465
-smtp_server = input("Введите адрес SMTP-Сервера: ")
-smtp_port = int(input("Введите порт SMTP-Сервера: "))
 
-e_adress = input("Введите свой адрес ЭП: ")
-e_pass = input("Введите пароль ЭП: ")
+    smtp_server = #input("Введите адрес SMTP-Сервера: ")
+    smtp_port = #int(input("Введите порт SMTP-Сервера: "))
 
-e_text = input("Введите текст отправки")
+    e_adress = #input("Введите свой адрес ЭП: ")
+    e_pass = #input("Введите пароль ЭП: ")
 
-smtpObj = smtplib.SMTP(smtp_server,smtp_port)
+    e_text = #input("Введите текст отправки")
 
-smtpObj.starttls()
+    def connection():
+        smtpObj = smtplib.SMTP(smtp_server,smtp_port)
 
-smtpObj.login(e_adress,e_pass)
+        smtpObj.starttls()
 
-f = open('123.txt', 'r')
+        smtpObj.login(e_adress,e_pass)
+    def spam():
+        f = open('123.txt', 'r')
 
-line = f.readline()
-while line:
-    smtpObj.sendmail(e_adress,line,"")
-    print(line,end='')
-    line = f.readline()
-f.close
-
-print("Jobs Done!!!")
-
-
+        line = f.readline()
+        while line:
+            smtpObj.sendmail(e_adress,line,"")
+            print(line,end='')
+            line = f.readline()
+        f.close
+    def finish():
+        print("Jobs Done!!!")
